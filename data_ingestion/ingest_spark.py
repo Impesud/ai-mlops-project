7186f0d2fdb9f051993e2cd310170fea8eabd110
root <root@Impesud.>
2025-05-25 23:18:38 +0200

    Update ingest_spark.py

--- data_ingestion/ingest_spark.py
+++ data_ingestion/ingest_spark.py
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     spark = (
         SparkSession.builder
         .appName("IngestioneBigData") 
-        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.1")
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.3.6")
         .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
         .config("spark.hadoop.fs.s3a.access.key", os.environ["AWS_ACCESS_KEY_ID"])
         .config("spark.hadoop.fs.s3a.secret.key", os.environ["AWS_SECRET_ACCESS_KEY"])
