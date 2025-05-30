0ffafab2c996d096092267dc20dde61dbfeaa1dd
root <root@Impesud.>
2025-05-30 10:43:55 +0200

    Update CI-CD model action

--- scripts/get_latest_model_path.py
+++ scripts/get_latest_model_path.py
@@ -6,20 +6,20 @@ def find_latest_model_artifact():
     matches = glob.glob("mlruns/*/*/artifacts/model", recursive=True)
 
     if not matches:
-        raise FileNotFoundError("❌ Nessun modello MLflow trovato in mlruns/*/*/artifacts/model")
+        raise FileNotFoundError("Nessun modello MLflow trovato in mlruns/*/*/artifacts/model")
 
     # Ordina per ultima modifica
     latest_model_dir = max(matches, key=os.path.getmtime)
     abs_path = os.path.abspath(latest_model_dir)
 
-    print(f"📁 Ultima directory modello: {abs_path}")
+    print(f"Ultima directory modello: {abs_path}")
 
     # Controlla se esiste il file MLmodel
     mlmodel_file = os.path.join(abs_path, "MLmodel")
     if os.path.isfile(mlmodel_file):
-        print(f"📄 File MLmodel: {mlmodel_file}")
+        print(f"File MLmodel: {mlmodel_file}")
     else:
-        print("⚠️ Nessun file MLmodel trovato.")
+        print("Nessun file MLmodel trovato.")
 
     # Cerca eventuali file di modello salvato (es. .pkl, .onnx, ecc.)
     model_files = glob.glob(os.path.join(abs_path, "*.*"))
